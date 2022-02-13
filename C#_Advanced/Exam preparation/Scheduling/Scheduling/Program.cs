1aea88cb9fc375a46d4dd8c9220753015baf8183
Ivan Ivanov <fenix4win@gmail.com>
2022-02-13 22:28:05 +0200

    Add Scheduling

---
+++ C#_Advanced/Exam preparation/Scheduling/Scheduling/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Scheduling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+          Stack<int> tasks =new Stack<int>( Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+
+
+            Queue<int> threads =new Queue<int>( Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+
+            int targetTask = int.Parse(Console.ReadLine());
+
+            while (true)
+            {
+                int currTask=tasks.Peek();
+                int currTread=threads.Peek();
+                if (currTask == targetTask)
+                {
+                    Console.WriteLine($"Thread with value {currTread} killed task {currTask}");
+                    Console.WriteLine(String.Join(" ",threads));
+                    break;
+                }
+
+                if (currTread>=currTask)
+                {
+                    tasks.Pop();
+                    threads.Dequeue();
+                }
+                else if (currTread<currTask)
+                {
+                    threads.Dequeue();
+                }
+
+            }
+
+
+        }
+    }
+}
